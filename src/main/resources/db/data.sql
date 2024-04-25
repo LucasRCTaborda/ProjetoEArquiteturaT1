2f10c43d60629ca1ed6ab4ca163c4616929f099f
LUCAS TABORDA <lucas_rubens_95@hotmail.com>
2024-04-24 21:45:38 -0300

    Crie um banco de dados para teste

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,15 +1,48 @@
-INSERT INTO produto (id,descricao,preco_unitario) VALUES (10,'Televisor',2000.0);
-INSERT INTO produto (id,descricao,preco_unitario) VALUES (20,'Geladeira',3500.0);
-INSERT INTO produto (id,descricao,preco_unitario) VALUES (30,'Fogao',1200.0);
-INSERT INTO produto (id,descricao,preco_unitario) VALUES (40,'Lava-louça',1800.0);
-INSERT INTO produto (id,descricao,preco_unitario) VALUES (50,'lava-roupas',2870.0);
 
-INSERT INTO item_de_estoque (id, produto_id, quantidade, estoque_min, estoque_max ) VALUES (100,10,20,5,50);
-INSERT INTO item_de_estoque (id, produto_id, quantidade, estoque_min, estoque_max ) VALUES (200,20,10,5,30);
-INSERT INTO item_de_estoque (id, produto_id, quantidade, estoque_min, estoque_max ) VALUES (300,40,8,5,50);
+INSERT INTO Aplicativo (id,nome,custoMensal) VALUES ('Icloud',15);
+INSERT INTO Aplicativo (id,nome,custoMensal) VALUES ('Netflix',45);
+INSERT INTO Aplicativo (id,nome,custoMensal) VALUES ('AcademiaLucão',120);
+INSERT INTO Aplicativo (id,nome,custoMensal) VALUES ('BarbarosBarbershop',150);
+INSERT INTO Aplicativo (id,nome,custoMensal) VALUES ('ifoodTeleFree',7.99);
 
 
 
-private long codigo;
-    private String nome;
-    private double custoMensal;
+INSERT INTO Usuario (nome,senha) VALUES ('Joao,1234');
+INSERT INTO Usuario (nome,senha) VALUES ('Felipe,1234');
+INSERT INTO Usuario (nome,senha) VALUES  ('John,1234');
+INSERT INTO Usuario (nome,senha) VALUES  ('Noah,1234');
+INSERT INTO Usuario (nome,senha) VALUES ('Patati,1234');
+
+
+
+INSERT INTO Cliente (codigo,nome,email) VALUES (10,'Joao','@joao');
+INSERT INTO Cliente (codigo,nome,email) VALUES (11,'Felipe','@Felipe');
+INSERT INTO Cliente (codigo,nome,email) VALUES (12,'John','@John');
+INSERT INTO Cliente (codigo,nome,email) VALUES (13,'Noah','@Noah');
+INSERT INTO Cliente (codigo,nome,email) VALUES (14,'Patati','@Patati');
+
+
+
+
+INSERT INTO Assinatura (codigo, aplicativo, cliente, inicioVigencia, fimVigencia)
+VALUES
+    (1, 'App1', 'Cliente1', '2024-04-25', '2024-04-30'),
+    (2, 'App2', 'Cliente2', '2024-04-26', '2024-05-01'),
+    (3, 'App3', 'Cliente3', '2024-04-27', '2024-05-02'),
+    (4, 'App4', 'Cliente4', '2024-04-28', '2024-05-03'),
+    (5, 'App5', 'Cliente5', '2024-04-29', '2024-05-04');
+
+
+
+INSERT INTO Pagamento (codigo, assinatura, valorPago, promocao)
+VALUES
+    (1, 1, 50.00, 'Promoção A'),
+    (2, 2, 45.00, 'Promoção B'),
+    (3, 3, 55.00, 'Promoção C'),
+    (4, 4, 60.00, 'Promoção D'),
+    (5, 5, 40.00, 'Promoção E');
+
+
+
+
+
