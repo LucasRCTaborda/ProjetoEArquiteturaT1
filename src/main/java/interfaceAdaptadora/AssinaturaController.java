db67b6b0e9b73324a67aaf027fa5b8b2372201a0
LUCAS TABORDA <lucas_rubens_95@hotmail.com>
2024-04-25 02:20:11 -0300

    criando mais precisa ser corrigido

--- src/main/java/interfaceAdaptadora/AssinaturaController.java
+++ src/main/java/interfaceAdaptadora/AssinaturaController.java
@@ -2,16 +2,58 @@ package interfaceAdaptadora;
 import dominio.modelos.Assinatura;
 import dominio.servicos.AssinaturaService;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/assinatura")
 public record AssinaturaController(AssinaturaService assinaturaService) {
-    @PostMapping
-    public Assinatura save(@RequestBody Assinatura assinatura){
-        return assinaturaService.save(assinatura);
+    @Autowired
+    public AssinaturaController(AssinaturaService assinaturaService) {
+        this.assinaturaService = assinaturaService;
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Assinatura> cadastrarAssinatura(@RequestBody Assinatura assinatura) {
+        Assinatura novaAssinatura = assinaturaService.cadastrarAssinatura(assinatura);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaAssinatura);
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<Assinatura> editarAssinatura(@PathVariable Long id, @RequestBody Assinatura assinaturaAtualizada) {
+        Assinatura assinaturaEditada = assinaturaService.editarAssinatura(id, assinaturaAtualizada);
+        if (assinaturaEditada != null) {
+            return ResponseEntity.ok(assinaturaEditada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Assinatura>> listarAssinaturas() {
+        List<Assinatura> assinaturas = assinaturaService.listarAssinaturas();
+        return ResponseEntity.ok(assinaturas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Assinatura> buscarAssinaturaPorId(@PathVariable Long id) {
+        Optional<Assinatura> assinatura = assinaturaService.buscarAssinaturaPorId(id);
+        return assinatura.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarAssinatura(@PathVariable Long id) {
+        Optional<Assinatura> assinatura = assinaturaService.buscarAssinaturaPorId(id);
+        if (assinatura.isPresent()) {
+            assinaturaService.delete(assinatura.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
