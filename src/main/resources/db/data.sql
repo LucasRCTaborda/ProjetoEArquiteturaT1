9f4291565ae8c626cc217f0175c2c5fe111ef651
LUCAS TABORDA <lucas_rubens_95@hotmail.com>
2024-04-29 14:16:45 -0300

    Usei inserção por banco de Dados.Sql dei um Create Table e testei h2 estã funcionando

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,20 @@
+create table Usuarios
+(
+    usuario LONG not null,
+    nome    VARCHAR  ,
+    senha   VARCHAR
+);
+
+
+
+INSERT INTO Usuarios (usuario, nome, senha) VALUES
+                                                (2, 'Maria', 'senha1'),
+                                                (3, 'Pedro', 'senha2'),
+                                                (4, 'Ana', 'senha3'),
+                                                (5, 'Lucas', 'senha4'),
+                                                (6, 'Julia', 'senha5'),
+                                                (7, 'Mateus', 'senha6'),
+                                                (8, 'Carla', 'senha7'),
+                                                (9, 'Fernanda', 'senha8'),
+                                                (10, 'Rafael', 'senha9'),
+                                                (11, 'Mariana', 'senha10');
